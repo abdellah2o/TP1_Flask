c0e17ad5c59ea0be98dcb0e289a5d5fb23229977
abdellah2o <abelaiboud@iut.univ-paris8.fr>
2026-03-14 00:17:50 +0100

    init jinja template file for the front-end development

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from controllers.courses_controller import courses_controller
 from controllers.gives_controller import gives_controller
 from controllers.teachers_controller import teachers_controller
+from services import teachers_service
 
 app = Flask(__name__)
 app.register_blueprint(teachers_controller)
@@ -11,8 +12,8 @@ app.register_blueprint(gives_controller)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template("index.html", teachers = teachers_service.teachers)
 
 
 if __name__ == '__main__':
