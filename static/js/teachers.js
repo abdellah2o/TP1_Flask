a7e5d31e7c4ed5b992f377f055cce5a52884324f
abdellah2o <abelaiboud@iut.univ-paris8.fr>
2026-03-14 21:55:44 +0100

    liaison back/front REST API for teachers module

--- static/js/teachers.js
+++ static/js/teachers.js
@@ -10,9 +10,40 @@ function addTeacher() {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
-        body: JSON.stringify({'id': 1, 'name': inputName.value, 'colleagues': colleagues})
+        body: JSON.stringify({'id': -1, 'name': inputName.value, 'colleagues': colleagues})
     }).then(r => r.json()))
 
     inputName.value = ''
     inputColleagues.value = ''
+}
+
+
+const inputId = document.getElementById("teacher-id")
+
+function deleteTeacher() {
+    console.log(fetch("api/teachers/" + inputId.value, {
+        method: "DELETE",
+        headers: {'Content-Type': 'application/json'}
+    }))
+
+    inputId.value = ''
+}
+
+
+const inputNamePut = document.getElementById("teacher-name-put")
+const inputColleaguesPut = document.getElementById("teacher-colleagues-put")
+const inputIdPut = document.getElementById("teacher-id-put")
+
+function updateTeacher() {
+    const colleaguesPut = inputColleaguesPut.value.split(",").map(v => v.trim()).filter(v => v !== "").map(Number)
+
+    console.log(fetch("api/teachers/" + inputIdPut.value, {
+        method: "PUT",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({'id': -1, 'name': inputNamePut.value, 'colleagues': colleaguesPut})
+    }).then(r => r.json()))
+
+    inputNamePut.value = ''
+    inputColleaguesPut.value = ''
+    inputIdPut.value = ''
 }
